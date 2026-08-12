0f28fab3d5b3b671d4f76d7a9f5b2cef38ca34bd
Benedict Sergio Astan <122471111+sergioastan@users.noreply.github.com>
2026-08-12 15:59:49 -0700

    did more. needs so much work

--- start.py
+++ start.py
@@ -3,18 +3,12 @@ import generate_auth
 
 from glob import glob
 
-from colorama import init as colorama_init
-from colorama import Fore
-from colorama import Style
-
-colorama_init(autoreset=True)
-
 # check for required files
 if not glob("main.conf"):
-    print(Fore.RED + "Missing main.conf! Please redownload from https://github.com/h-dew/steamoji_copyparty!")
+    print("Missing main.conf! Please redownload from https://github.com/h-dew/steamoji_copyparty!")
 
 if not glob("copyparty-sfx.py"):
-    print(Fore.RED + "Missing copyparty-sfx.py! Please redownload from https://github.com/h-dew/steamoji_copyparty")
+    print("Missing copyparty-sfx.py! Please redownload from https://github.com/h-dew/steamoji_copyparty")
 
 # to change apprentice folder name, can set APPRENTICE_FOLDER_NAME right here
 #  EX: generate_auth.APPRENTICE_FOLDER_NAME = "folder"
@@ -22,17 +16,27 @@ if not glob("copyparty-sfx.py"):
 
 # check if apprentice parent folder exists
 if not glob("../" + generate_auth.APPRENTICE_FOLDER_NAME):
-    print(Fore.RED + "Could not find apprentice container folder! Check this script and manually change APPRENTICE_FOLDER_NAME above if needed")
+    print("Could not find apprentice container folder! Check this script and manually change APPRENTICE_FOLDER_NAME above if needed")
 
 # generate auth
-print(Fore.YELLOW + "Generating users from apprentice folder: " + Fore.MAGENTA + generate_auth.APPRENTICE_FOLDER_NAME)
+print("Generating users from apprentice folder: " + generate_auth.APPRENTICE_FOLDER_NAME)
 generate_auth.generateConfFile()
 
 # double check that we have the users file
 if not glob("users.conf"):
-    print(Fore.RED + "Could not find users.conf... should've been generated earlier. Check apprentice folder layout or" + Fore.YELLOW + "generate_auth.py")
+    print("Could not find users.conf... should've been generated earlier. Check apprentice folder layout or" + " generate_auth.py")
 
 
 
+# TIME TO FORK BABY !!!
+pid = os.fork()
 
+# FINISH LATER
+path_to_python_binary = sys.executable
+
+args = ["python3", "/PATH"] # CHECK IF PATH IS RELATIVE
+
+env = os.environ
+
+os.execve(path_to_python_binary, args, env)
 
