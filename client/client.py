f9eaf969ba4f53e8941160d4f03c8b1dbf3707a3
aubrey <scownaubrey@gmail.com>
2026-08-20 15:49:04 -0700

    started working on client

---
+++ client/client.py
@@ -0,0 +1,11 @@
+# TRAY APP
+# Stays alive in tray, 
+
+
+# FEATURES
+# Tray icon colour shows connectivity
+
+# Disconnect all connections at 11pm
+
+# Logout on tray right click
+# Maybe a login option too? idek
---
+++ client/connect.py
@@ -0,0 +1,69 @@
+import socket
+import os
+import subprocess
+from string import ascii_uppercase
+
+# Get current drives using modern os.listdrives() (Python 3.12+)
+# Fall back to checking existence if on older versions... I like the other way better
+def getEmptyDrive():
+    if hasattr(os, "listdrives"):
+        used_drives = {d[0].upper() for d in os.listdrives()}
+    else:
+        used_drives = {
+            chr(i) for i in range(65, 91) if os.path.exists(chr(i) + ":\\")
+        }
+
+    # All letters from A to Z
+    all_letters = set(ascii_uppercase)
+
+    # Find unused letters
+    free_letters = sorted(all_letters - used_drives)
+
+    # Pick the first available empty slot letter
+    empty_slot = free_letters[0] if free_letters else None
+
+    if empty_slot == None:
+        raise OSError("No open drive slots! System should be rebooted and non-essential extra drives should be removed")
+
+    return empty_slot
+
+def getIp(hostname)
+    # Get IP of server
+    # mDNS baybeeee!!
+    return socket.gethostbyname(hostname)
+
+
+def generateMountCommand(hostname, volume):
+    mntlocation = getEmptyDrive()
+
+    return f"rclone mount --vfs-cache-mode writes --dir-cache-time 5s --network-mode {hostname}-dav:{volume} {mntlocation}:".split()
+
+def generateConfig(hostname, username, password):
+    hosturl = "http://" + getIp(hostname)
+
+    return f"rclone config create {hostname}-dav webdav url={hosturl} vendor=owncloud pacer_min_sleep=0.01ms user={username} pass={password}".split()
+
+def connect(username, password, host, volume):
+    # Generates Rclone config, then connects.
+    # Returns 0 on success
+
+    if len(host) < 1:
+        # just a fallback in case
+        host = "StoragePC"
+        print("No host specified, using default: StoragePC")
+
+    try:
+        # Check that host resolves
+        ip = getIp(host)
+
+    except:
+        print("Could not resolve host's IP! Check hostname.")
+        return 1
+
+    ## NVMMM WE USING POPEN
+    
+    #subprocess.run(generateConfig(host, username, password))
+    #subprocess.run(generateMountCommand(host, volume))
+
+def connectApprentice(username, host):
+    # Make 2 connections, 
